fdbd82e269165c02fcb6fe3f413d2be8a8be8580
Aqpcet <792286042@qq.com>
2019-08-31 20:42:30 +0800

    修改gitPush

--- gitPush.sh
+++ gitPush.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-echo "git pull origin master"
+echo "+git pull origin master"
 git pull origin master
-echo -e "\n"
-echo "=====ヾ(≧O≦)〃嗷~====="
 
-echo "git add ."
+
+echo "+git add ."
 git add .
-echo -e "\n"
-echo "=====ヾ(≧O≦)〃嗷~====="
 
-echo "请输入提交信息:"
+echo "请输入提交信息："
 read message
-echo -e "\n"
-echo "=====ヾ(≧O≦)〃嗷~====="
 
-echo "git commit -m "$message""
+
+echo "+git commit -m "$message""
 git commit -m "$message"
-echo -e "\n"
-echo "=====ヾ(≧O≦)〃嗷~====="
 
-echo "git push origin master"
+
+echo "+git push origin master"
 git push origin master
